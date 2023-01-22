2b6f0bc9c011c44121986fbc6fdbedc0c8d5bd3c
Yunhao Qian <yunhaoqian@hotmail.com>
2023-01-21 19:44:32 -0500

    Add page support

---
+++ index.js
@@ -0,0 +1,35 @@
+const topContainer = document.getElementById("top-container");
+const previousButton = document.getElementById("previous-button");
+const nextButton = document.getElementById("next-button");
+
+const clockPageIDs = [
+  "point-towards-sun-page",
+  "bisector-pointing-south-page",
+  "real-south-direction-page",
+  "error-sun-direction-page",
+  "error-local-time-page",
+  "error-system-error-page",
+];
+const mainPageIDs = ["introduction-page"].concat(clockPageIDs, ["thank-you-page"]);
+const allPageIDs = ["splash-page"].concat(mainPageIDs);
+
+function goToPageWithID(pageID) {
+  topContainer.classList.remove("at-clock-pages", ...allPageIDs.map(id => "at-" + id));
+  topContainer.classList.add("at-" + pageID);
+  if (clockPageIDs.includes(pageID)) {
+    topContainer.classList.add("at-clock-pages");
+  }
+  const index = mainPageIDs.indexOf(pageID);
+  if (index === 0) {
+    previousButton.disabled = true;
+  } else {
+    previousButton.disabled = false;
+    previousButton.onclick = () => goToPageWithID(mainPageIDs[index - 1]);
+  }
+  if (index === mainPageIDs.length - 1) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+    nextButton.onclick = () => goToPageWithID(mainPageIDs[index + 1]);
+  }
+}
